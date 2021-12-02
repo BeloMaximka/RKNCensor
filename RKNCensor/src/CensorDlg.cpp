c234ff55333a12c59120715bae73d34b892fabe6
BeloMaximka <belomaxim@gmail.com>
2021-12-02 16:22:54 +0200

    simple word adding (uniqueness check needed)

--- RKNCensor/src/CensorDlg.cpp
+++ RKNCensor/src/CensorDlg.cpp
@@ -12,10 +12,23 @@ BOOL CensorDlg::OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
 	return TRUE;
 }
 
-void CensorDlg::OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+void CensorDlg::Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 {
 	switch (id)
 	{
+	case IDC_ADDWORD_BTN:
+		AddWord(hwnd);
+		break;
+	}
+}
+
+void CensorDlg::AddWord(HWND hwnd)
+{
+	TCHAR word[256];
+	GetDlgItemText(hwnd, IDC_WORD_EDIT, word, 256);
+	if (_tcslen(word) && SendDlgItemMessage(hwnd, LB_FINDSTRINGEXACT, LB_ADDSTRING, -1, LPARAM(word)) == 0)
+	{
+		SendDlgItemMessage(hwnd, IDC_CENSOR_LIST, LB_ADDSTRING, 0, LPARAM(word));
 	}
 }
 
@@ -30,7 +43,7 @@ BOOL CensorDlg::DlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 		HANDLE_MSG(hwnd, WM_CLOSE, ptr->OnClose);
 		HANDLE_MSG(hwnd, WM_INITDIALOG, ptr->OnInitDialog);
-		HANDLE_MSG(hwnd, WM_COMMAND, ptr->OnCommand);
+		HANDLE_MSG(hwnd, WM_COMMAND, ptr->Cls_OnCommand);
 	}
 	return FALSE;
 }
--- RKNCensor/src/CensorDlg.h
+++ RKNCensor/src/CensorDlg.h
@@ -9,7 +9,9 @@ class CensorDlg
 	static CensorDlg* ptr;
 	void OnClose(HWND hwnd);
 	BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam);
-	void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+	void Cls_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+
+	void AddWord(HWND hwnd);
 public:
 	CensorDlg();
 	//~CensorDlg();
